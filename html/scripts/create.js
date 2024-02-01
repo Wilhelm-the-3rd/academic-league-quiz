c8b096d11b6de54e9810ad6fa3b952f76452231d
Will <williampetersmeyer@gmail.com>
2024-01-31 20:20:50 -0800

    Added question creator

---
+++ html/scripts/create.js
@@ -0,0 +1,35 @@
+function getElement(id) { return document.getElementById(id); }
+function setText(element, newText) { element.textContent = newText; }
+
+const category = getElement("category-text")
+    , question = getElement("question-text")
+    , answers = getElement("answers-text")
+    , difficulty = getElement("difficulty-text")
+    , author = getElement("author-text");
+
+async function copy() {
+    try {
+        await navigator.clipboard.writeText(getElement("text-output").textContent);
+    } catch (e) {
+        console.error('Failed to copy: ', e);
+    }
+}
+
+function returnQuestion() {
+    let answers_formatted = "[";
+
+    answers.value.split(",").forEach((element) => answers_formatted += '"' + element.trim() + '",');
+
+    answers_formatted += "]";
+
+    let output = category.value.trim() + ";" + question.value.trim() + ";" + answers_formatted + ";" + difficulty.value.trim() + ";" + author.value.trim();
+
+    setText(getElement("text-output"), output);
+
+}
+
+getElement("category-text").addEventListener("input", returnQuestion);
+getElement("question-text").addEventListener("input", returnQuestion);
+getElement("answers-text").addEventListener("input", returnQuestion);
+getElement("difficulty-text").addEventListener("input", returnQuestion);
+getElement("author-text").addEventListener("input", returnQuestion);
